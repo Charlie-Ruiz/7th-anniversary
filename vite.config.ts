3a998fe00d6c76ebc439fae7b627af80fa56194d
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2025-11-09 12:16:01 -0500

    Updat 1

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/7th-anniversary/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
