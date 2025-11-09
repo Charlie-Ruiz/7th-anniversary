c29484ca1088bd8017289f19d7d201331bbc653f
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2025-11-09 12:01:02 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Timeline from "./pages/Timeline";
 import Gallery from "./pages/Gallery";
